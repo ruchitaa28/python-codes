2f6242ffd89b60dc7de328b1c37b7a2d1ffbf926
ruchitaa28 <ruchitaavaidya@gmail.com>
2025-04-23 11:56:10 +0530

    Add files via upload

---
+++ dict2.py
@@ -0,0 +1,9 @@
+thisdict = {
+  "brand":"Ford",
+  "model":"Mustang",
+  "year": 1964,
+  "gaag":''
+}
+for k,v in thisdict.items():
+    if k=="" or v=="" :
+        print("key or value is empty",k)
